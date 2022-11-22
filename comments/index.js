e013b15a42c6332a92c29a1cc7772b4e8845906e
Hasan Afridi <hasan.afridi@outlook.com>
2022-11-22 16:21:19 +0000

    create event from post and comment service

--- comments/index.js
+++ comments/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,14 +14,28 @@ app.get("/posts/:id/comments", (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
   const commentId = randomBytes(4).toString("hex");
   const { content } = req.body;
+  const { id } = req.params;
 
-  const comments = commentsByPostId[req.params.id] || [];
+  const comments = commentsByPostId[id] || [];
   comments.push({ id: commentId, content });
 
-  commentsByPostId[req.params.id] = comments;
+  commentsByPostId[id] = comments;
+
+  await axios
+    .post("http://localhost:4005/events", {
+      type: "CommentCreated",
+      data: {
+        id: commentId,
+        content,
+        postId: id,
+      },
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 
   res.status(201).send(comments);
 });
