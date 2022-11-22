44c71079d052e96f4bbb5c78077aafec25f80a4c
Hasan Afridi <hasan.afridi@outlook.com>
2022-11-22 16:06:03 +0000

    create event-bus

---
+++ event-bus/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post("/events", async (req, res) => {
+  const event = req.body;
+
+  await axios.post("http://localhost:4000/events", event).catch((err) => {
+    console.log(err);
+  });
+  await axios.post("http://localhost:4001/events", event).catch((err) => {
+    console.log(err);
+  });
+  await axios.post("http://localhost:4002/events", event).catch((err) => {
+    console.log(err);
+  });
+
+  res.send({ status: "ok" });
+});
+
+app.listen(4005, () => {
+  console.log("listening on 4005");
+});
