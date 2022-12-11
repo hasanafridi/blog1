04518c711901b6807ae585a054daafdac63bf1f8
Hasan Afridi <hasan.afridi@outlook.com>
2022-12-11 17:52:32 +0000

    update routes to use pods

--- event-bus/index.js
+++ event-bus/index.js
@@ -12,16 +12,18 @@ app.post("/events", async (req, res) => {
 
   events.push(event);
 
-  await axios.post("http://localhost:4000/events", event).catch((err) => {
+  await axios
+    .post("http://posts-clusterip-srv:4000/events", event)
+    .catch((err) => {
+      console.log(err);
+    });
+  await axios.post("http://comments-srv:4001/events", event).catch((err) => {
     console.log(err);
   });
-  await axios.post("http://localhost:4001/events", event).catch((err) => {
+  await axios.post("http://query-srv:4002/events", event).catch((err) => {
     console.log(err);
   });
-  await axios.post("http://localhost:4002/events", event).catch((err) => {
-    console.log(err);
-  });
-  await axios.post("http://localhost:4003/events", event).catch((err) => {
+  await axios.post("http://moderation-srv:4003/events", event).catch((err) => {
     console.log(err);
   });
 
